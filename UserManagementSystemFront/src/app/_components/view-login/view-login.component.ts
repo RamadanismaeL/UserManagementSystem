fbc875bf813f4980e9171e74b8989c3b36121fd5
RamadanismaeL <ramadan.ismael02@gmail.com>
2025-02-03 22:53:46 +0200

    updated

---
+++ UserManagementSystemFront/src/app/_components/view-login/view-login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-login',
+  imports: [],
+  templateUrl: './view-login.component.html',
+  styleUrl: './view-login.component.scss'
+})
+export class ViewLoginComponent {
+
+}
--- UserManagementSystemFront/src/app/_pages/login/login.component.ts
+++ UserManagementSystemFront/src/app/_pages/login/login.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../_components/footer/footer.component';
+import { ViewLoginComponent } from '../../_components/view-login/view-login.component';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ FooterComponent, ViewLoginComponent ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
