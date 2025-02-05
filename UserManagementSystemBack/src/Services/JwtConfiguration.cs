2a794c7055b1beff2968710be8e729f69c154405
RamadanismaeL <ramadan.ismael02@gmail.com>
2025-02-05 11:35:23 +0200

    cookies udpated

--- UserManagementSystemBack/src/Services/JwtConfiguration.cs
+++ UserManagementSystemBack/src/Services/JwtConfiguration.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 
@@ -42,6 +43,21 @@ namespace UserManagementSystemBack.src.Services
                     IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(getSigningKey)),
                     RequireSignedTokens = true,
                     ClockSkew = TimeSpan.FromMilliseconds(900)
+                };
+            });
+            service.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options =>
+            {
+                options.Cookie.HttpOnly = true;        // Impede o acesso via JavaScript
+                options.Cookie.SecurePolicy = CookieSecurePolicy.Always; // Só envia cookies em conexões HTTPS
+                options.Cookie.SameSite = SameSiteMode.Strict; // Previne CSRF
+                options.Cookie.Name = "UserManagementSystemCookie";    // Nome personalizado para o cookie
+                options.Cookie.MaxAge = TimeSpan.FromMinutes(30);  // Expira após 30 minutos de inatividade
+                options.SlidingExpiration = true;  // Renova a expiração a cada requisição
+                options.Events.OnSigningOut = context =>
+                {
+                    context.Response.Cookies.Delete("UserManagementSystemCookie");
+                    return Task.CompletedTask;
                 };
             });
         }
