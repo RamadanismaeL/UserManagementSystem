5e403c1b5fbe2c3c56596009dfb5c05d17a41a4d
RamadanismaeL <ramadan.ismael02@gmail.com>
2025-02-17 21:43:25 +0200

    updated

--- UserManagementSystemBack/obj/Debug/net9.0/UserManagementSystemBack.AssemblyInfo.cs
+++ UserManagementSystemBack/obj/Debug/net9.0/UserManagementSystemBack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagementSystemBack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c183d3156171696fe53dc26e789bbfa879530eb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d0763cb51c44de262fa376da3690308ef89c9bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagementSystemBack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagementSystemBack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
